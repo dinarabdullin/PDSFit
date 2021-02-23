5af416181af22a4e18185516dd0b19d549027b1b
Dinar Abdullin <dinarabdullinkzn@gmail.com>
2021-02-23 16:59:24 +0100

    update

--- experiments/experiment_types.py
+++ experiments/experiment_types.py
@@ -7,3 +7,4 @@ from experiments.peldor_4p_rect import Peldor_4p_rect
 
 experiment_types = {}
 experiment_types["4pELDOR-rect"] = Peldor_4p_rect
+experiment_types["5pRIDME-rect"] = Ridme_5p_rect
--- simulation/simulator.py
+++ simulation/simulator.py
@@ -14,35 +14,41 @@ class Simulator():
 
     def __init__(self, calculation_settings):
         self.integration_method = calculation_settings["integration_method"]
+        if self.integration_method == "monte_carlo":
+            self.separate_grids = False
+        else:
+            self.separate_grids = True
         self.mc_sample_size = calculation_settings["mc_sample_size"]
         self.grid_size = calculation_settings["grid_size"]
+        self.distributions = calculation_settings["distributions"]
         self.excitation_threshold = calculation_settings["excitation_treshold"]
         self.frequency_increment_epr_spectrum = 0.001 # in GHz
-        self.field_directions = []
-    
-    def set_field_directions(self):
-        field_directions = []
+        self.field_orientations = []
+        self.euler_angles_convention = "ZXZ" 
+        
+    def field_orientations(self):
+        field_orientations = []
         if self.integration_method == "monte_carlo":
-            field_directions = random_points_on_sphere(self.mc_sample_size)
+            field_orientations = random_points_on_sphere(self.mc_sample_size)
         elif self.integration_method == "uniform_grids":
             ## Fibinacci grid points
-            #field_directions = fibonacci_grid_points(self.grid_size["powder_averaging"])
+            #field_orientations = fibonacci_grid_points(self.grid_size["powder_averaging"])
             # Golden spiral grid points
-            field_directions = golden_spiral_grid_points(self.grid_size["powder_averaging"])
-        return field_directions
-    
-    def epr_spectrum(self, spin_system, field_value):
+            field_orientations = golden_spiral_grid_points(self.grid_size["powder_averaging"])
+        return field_orientations
+
+    def epr_spectrum(self, spins, field_value):
         # Random orientations of the static magnetic field
-        if self.field_directions == []:
-            self.field_directions = self.set_field_directions()
-        num_field_directions = self.field_directions.shape[0]
+        if self.field_orientations == []:
+            self.field_orientations = self.field_orientations()
+        num_field_orientations = self.field_orientations.shape[0]
         # Resonance frequencies and their probabilities
         all_frequencies = []
         all_probabilities = []
-        for spin in spin_system:
+        for spin in spins:
             # Resonance frequencies
-            resonance_frequencies = spin.res_freq(self.field_directions, field_value)
-            weights = np.tile(spin.int_res_freq, (num_field_directions,1))
+            resonance_frequencies = spin.res_freq(self.field_orientations, field_value)
+            weights = np.tile(spin.int_res_freq, (num_field_orientations,1))
             # Frequency ranges
             min_resonance_frequency = np.amin(resonance_frequencies)
             max_resonance_frequency = np.amax(resonance_frequencies)
@@ -58,5 +64,16 @@ class Simulator():
         spectrum["f"] = np.arange(min_frequency, max_frequency+self.frequency_increment_epr_spectrum, self.frequency_increment_epr_spectrum)
         spectrum["s"] = histogram(all_frequencies, bins=spectrum["f"], weights=all_probabilities)
         return spectrum
-        
     
+    # IN PROGRESS
+    def simulate_time_trace(self, experiment, spins, variables):
+        time_trace = {}
+        time_trace["t"] = experiment.t
+        if len(spins) == 2:
+            pass # put your simulation code here instead of pass
+        elif len(spins) == 3:
+            pass
+        else:
+            raise ValueError('Invalid number of spins! Currently the number of spins is limited by 2 or 3.')
+            sys.exit(1)
+        return time_trace
