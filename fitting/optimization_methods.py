22f9298888b360c36127a09c178526bea1f713a2
Dinar Abdullin <dinarabdullinkzn@gmail.com>
2021-08-10 11:01:35 +0200

    update

--- fitting/optimization_methods.py
+++ fitting/optimization_methods.py
@@ -3,9 +3,7 @@
 from fitting.optimizer import Optimizer
 from fitting.ga.ga import GeneticAlgorithm
 from fitting.ga_local.ga_local import GeneticAlgorithmWithLocalSolver
-from fitting.ga_sa_local.ga_sa_local import GeneticAlgorithmWithSimulatedAnnealingAndLocalSolver
 
 optimization_methods = {}
 optimization_methods['ga'] = GeneticAlgorithm
 optimization_methods['ga_local'] = GeneticAlgorithmWithLocalSolver
-optimization_methods['ga_sa_local'] = GeneticAlgorithmWithSimulatedAnnealingAndLocalSolver
